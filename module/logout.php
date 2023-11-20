152caad569ba439c8c30b347e0df11ed9d2ba519
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-20 10:57:37 -0300

    evitar chamadas desnecessárias ao script de logout

--- module/logout.php
+++ module/logout.php
@@ -1,7 +1,14 @@
 <?php
 // logout.php
 session_start();
-session_destroy();
+
+// Verificar se o usuário está logado antes de destruir a sessão
+if (isset($_SESSION["logado"]) && $_SESSION["logado"]) {
+    // Destruir a sessão
+    session_destroy();
+}
+
+// Redirecionar para a página de login
 header("Location: ../index.php");
 exit();
 ?>
