b7b02fe3c6029fe6f00d59c91c436bb827a4ec63
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-20 14:42:39 -0300

    Pequenas alterações

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ define('DB_USER', 'ragnarok');          // Usuário do Banco de dados
 define('DB_PASSWORD', 'ragnarok');      // Senha do Banco de dados
 define('DB_NAME', 'ragnarok');          // Nome do Banco de dados
 
-define('SITE_TITLE', 'Lumen Flux');     // Título do Site
+define('SITE_TITLE', 'Flex CP');     // Título do Site
 define('ENVIO_DISCORD_ATIVADO', true);  // Defina como true para ativar ou false para desativar
                                         // o envio de Mensagem de nova conta no Servidor do Discord.
 define('DISCORD_WEBHOOK_URL', 'https://discord.com/api/webhooks/SEU_WEBHOOK_ID/SEU_TOKEN');
