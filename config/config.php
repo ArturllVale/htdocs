2ac35fcaa40908e1aefb3e6e305cc655aa2d056e
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-21 09:29:37 -0300

    Ajustes de Comentários

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
-define('DB_HOST', 'localhost');         // IP do Host ou localhost
-define('DB_USER', 'ragnarok');          // Usuário do Banco de dados
-define('DB_PASSWORD', 'ragnarok');      // Senha do Banco de dados
-define('DB_NAME', 'ragnarok');          // Nome do Banco de dados
+define('DB_HOST', 'localhost');             // IP do Host ou localhost
+define('DB_USER', 'ragnarok');              // Usuário do Banco de dados
+define('DB_PASSWORD', 'ragnarok');          // Senha do Banco de dados
+define('DB_NAME', 'ragnarok');              // Nome do Banco de dados
 
-define('SITE_TITLE', 'Flex CP');        // Título do Site
-define('ENVIO_DISCORD_ATIVADO', false);  // Defina como true para ativar ou false para desativar
-                                        // o envio de Mensagem de nova conta no Servidor do Discord.
+define('SITE_TITLE', 'Flex CP');            // Título do Site
+define('ENVIO_DISCORD_ATIVADO', false);     // Defina como true para ativar ou false para desativar
+                                            // o envio de Mensagem de nova conta no Servidor do Discord.
 define('DISCORD_WEBHOOK_URL', 'https://discord.com/api/webhooks/SEU_WEBHOOK_ID/SEU_TOKEN');
 ?>
