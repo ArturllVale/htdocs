b5439ab7fb1e61bf77ab4c8d64266546ae52d827
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 14:09:45 -0300

    Update

--- pages/adm.php
+++ pages/adm.php
@@ -7,3 +7,16 @@ if (!isset($_SESSION["logado"]) || $_SESSION["logado"] !== true) {
 }
 ?>
 <h2>Administração do Servidor</h2>
+<p class="d-inline-flex gap-1">
+  <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Link with href
+  </a>
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    Button with data-bs-target
+  </button>
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+  </div>
+</div>
