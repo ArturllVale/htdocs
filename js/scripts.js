f47fdb8aa60050126e5f3e2cf579d1ec37805f13
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 12:09:55 -0300

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("cookieConsentPopup").style.display = "block";
     }
 
-    // Carregar o nome de usuário do armazenamento local, se disponível
-    var usuarioKey = "usuario";
-    var usuarioInput = document.getElementById("usuarioInput");
-    var salvarUsuarioCheckbox = document.getElementById("salvarUsuarioCheckbox");
-    if (localStorage.getItem(usuarioKey)) {
-        usuarioInput.value = localStorage.getItem(usuarioKey);
-        salvarUsuarioCheckbox.checked = true;
-    }
-
     // Salvar o nome de usuário no armazenamento local quando o formulário é enviado
     document.querySelector("form").addEventListener("submit", function() {
         if (salvarUsuarioCheckbox.checked) {
@@ -28,7 +19,15 @@ document.addEventListener("DOMContentLoaded", function() {
             localStorage.removeItem(usuarioKey);
         }
     });
-});
 
+    // Carregar o nome de usuário do armazenamento local, se disponível
+    var usuarioKey = "usuario";
+    var usuarioInput = document.getElementById("usuarioInput");
+    var salvarUsuarioCheckbox = document.getElementById("salvarUsuarioCheckbox");
+    if (localStorage.getItem(usuarioKey)) {
+        usuarioInput.value = localStorage.getItem(usuarioKey);
+        salvarUsuarioCheckbox.checked = true;
+    }
+});
 
 document.getElementById("acceptCookiesButton").addEventListener("click", aceitarCookies);
