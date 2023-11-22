66d28cf8bff0ec69d28a8f59af5373abf5ae5fcf
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 14:45:35 -0300

    Update

---
+++ js/functions.js
@@ -0,0 +1,25 @@
+// Oculta todos os conteúdos inicialmente
+window.onload = function() {
+    var allContents = document.getElementsByClassName('content');
+    for (var i = 0; i < allContents.length; i++) {
+      allContents[i].style.display = 'none';
+    }
+};
+
+function toggleCollapse(btnNumber) {
+    var contentId = 'content' + btnNumber;
+    var content = document.getElementById(contentId);
+
+    // Oculta todos os conteúdos
+    var allContents = document.getElementsByClassName('content');
+    for (var i = 0; i < allContents.length; i++) {
+      allContents[i].style.display = 'none';
+    }
+
+    // Mostra ou oculta o conteúdo clicado
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+}
