02ffa80101e167e2c50bfbd71546452aaffea290
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 10:47:31 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');             // IP do Host ou localhost
 define('DB_USER', 'ragnarok');              // Usuário do Banco de dados
-define('DB_PASSWORD', 'ragnarok');          // Senha do Banco de dados
+define('DB_PASSWORD', 'ragnarok3229');          // Senha do Banco de dados
 define('DB_NAME', 'ragnarok');              // Nome do Banco de dados
 
 define('SITE_TITLE', 'Flex CP');            // Título do Site
