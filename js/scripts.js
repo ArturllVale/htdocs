295726605b1c6fd792400f60e3770b705be00313
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 12:03:13 -0300

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
-function aceitarCookies() {
-    var cookieKey = "aceitouCookies";
-    var preferenciaAceite = "sim";
-    localStorage.setItem(cookieKey, preferenciaAceite);
-    document.getElementById("cookieConsentPopup").style.display = "none";
-}
-
 document.addEventListener("DOMContentLoaded", function() {
     var cookieKey = "aceitouCookies";
     if (localStorage.getItem(cookieKey) !== "sim") {
@@ -15,20 +8,23 @@ document.addEventListener("DOMContentLoaded", function() {
     var usuarioKey = "usuario";
     var usuarioInput = document.getElementById("usuarioInput");
     var salvarUsuarioCheckbox = document.getElementById("salvarUsuarioCheckbox");
-    if (localStorage.getItem(usuarioKey)) {
-        usuarioInput.value = localStorage.getItem(usuarioKey);
-        salvarUsuarioCheckbox.checked = true;
-    }
 
-    // Salvar o nome de usuário no armazenamento local quando o formulário é enviado
-    document.querySelector("form").addEventListener("submit", function() {
-        if (salvarUsuarioCheckbox.checked) {
-            localStorage.setItem(usuarioKey, usuarioInput.value);
-        } else {
-            localStorage.removeItem(usuarioKey);
+    if (usuarioInput) {
+        // Verificar se o elemento existe antes de acessá-lo
+        if (localStorage.getItem(usuarioKey)) {
+            usuarioInput.value = localStorage.getItem(usuarioKey);
+            salvarUsuarioCheckbox.checked = true;
         }
-    });
-});
 
+        // Salvar o nome de usuário no armazenamento local quando o formulário é enviado
+        document.querySelector("form").addEventListener("submit", function() {
+            if (salvarUsuarioCheckbox.checked) {
+                localStorage.setItem(usuarioKey, usuarioInput.value);
+            } else {
+                localStorage.removeItem(usuarioKey);
+            }
+        });
+    }
+});
 
 document.getElementById("acceptCookiesButton").addEventListener("click", aceitarCookies);
