3512224c9ed7281e7e806e66b7fea1a771d44e04
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 15:00:21 -0300

    Update

--- pages/adm.php
+++ pages/adm.php
@@ -8,6 +8,9 @@ if (!isset($_SESSION["logado"]) || $_SESSION["logado"] !== true) {
 ?>
 <h2>Administração do Servidor</h2>
 <button class="btn btn-primary" type="button" onclick="toggleCollapse(1)">Botão 1</button>
+<button class="btn btn-primary" type="button" onclick="toggleCollapse(2)">Botão 2</button>
+
+
 <div class="content" id="content1">
     <div class="card card-body">
         Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user
@@ -15,7 +18,6 @@ if (!isset($_SESSION["logado"]) || $_SESSION["logado"] !== true) {
     </div>
 </div>
 
-<button class="btn btn-primary" type="button" onclick="toggleCollapse(2)">Botão 2</button>
 <div class="content" id="content2">
     <div class="card card-body">
         Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user
