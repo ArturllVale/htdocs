2e63a8495c2d053d9192fa433841a27c39b3e7ab
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 14:23:57 -0300

    Update

--- index.php
+++ index.php
@@ -54,12 +54,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <p class="mb-0">Este site usa cookies para garantir que você obtenha a melhor experiência em nosso site. <button id="acceptCookiesButton" class="btn btn-primary btn-sm">Aceitar</button></p>
     </div>
 </div>
-
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="js/scripts.js"></script>
-  
-</body>
-
-</html>
+<?php 
+  include "footer.php";
+?>
