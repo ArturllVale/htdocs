8d6f0126d141d1e63a33631e08262e4b82704523
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 12:49:26 -0300

    update

--- module/menu.php
+++ module/menu.php
@@ -19,14 +19,12 @@ if (!isset($_SESSION["logado"]) || $_SESSION["logado"] == true) {
             <li class="breadcrumb-item"><a href="?page=rank_pvp">Rank PVP</a></li>
             <li class="breadcrumb-item"><a href="?page=rank_mvp">Rank MVP</a></li>
             <li class="breadcrumb-item"><a href="?page=mvps_vivo">MVPs Vivo</a></li>
-            <div class="dropdown">
-            <li class="breadcrumb-item"><a type="button" data-bs-toggle="dropdown" aria-expanded="false">Administração</a></li>
+            <li class="breadcrumb-item dropdown"><a type="button" data-bs-toggle="dropdown" aria-expanded="false">Administração</a></li>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="#">Action</a></li>
               <li><a class="dropdown-item" href="#">Another action</a></li>
               <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
-            </div>
             <li class="breadcrumb-item"><a href="module/logout.php">Sair</a></li>
         </ol>
     </nav>
