b820710332806c34b7c8215ffa1513ab03353a58
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 14:25:35 -0300

    Update

--- index.php
+++ index.php
@@ -3,20 +3,20 @@ session_start();
 include_once("config/includes.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $usuario = $_POST["usuario"];
-    $senha = $_POST["senha"];
-    $salvarUsuario = isset($_POST["salvarUsuario"]);
+  $usuario = $_POST["usuario"];
+  $senha = $_POST["senha"];
+  $salvarUsuario = isset($_POST["salvarUsuario"]);
 
-    if (verificar_login($usuario, $senha, $salvarUsuario)) {
-        $_SESSION["sex"] = obterGeneroDoUsuario($usuario);
-        $_SESSION["logado"] = true;
-        header("Location: index.php");
-        exit();
-    } else {
-        $_SESSION["erro_login"] = 'Falha no login. Tente novamente.';
-        header("Location: index.php");
-        exit();
-    }
+  if (verificar_login($usuario, $senha, $salvarUsuario)) {
+    $_SESSION["sex"] = obterGeneroDoUsuario($usuario);
+    $_SESSION["logado"] = true;
+    header("Location: index.php");
+    exit();
+  } else {
+    $_SESSION["erro_login"] = 'Falha no login. Tente novamente.';
+    header("Location: index.php");
+    exit();
+  }
 }
 ?>
 
@@ -32,28 +32,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta name="version" content="1.0">
   <link rel="shortcut icon" href="Favicon.ico" type="image/x-icon">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php echo SITE_TITLE; ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title>
+    <?php echo SITE_TITLE; ?>
+  </title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="css/estilo.css">
 </head>
 
 <body>
   <br>
   <br>
-  <?php 
-    if (isset($_SESSION["logado"]) && $_SESSION["logado"]) {
-        include "module/main.php";
-    } else {
-        include "module/login.php";
-    }
+  <?php
+  if (isset($_SESSION["logado"]) && $_SESSION["logado"]) {
+    include "module/main.php";
+  } else {
+    include "module/login.php";
+  }
   ?>
 
-<div id="cookieConsentPopup" class="fixed-bottom p-3 bg-light" style="display: none;">
+  <div id="cookieConsentPopup" class="fixed-bottom p-3 bg-light" style="display: none;">
     <div class="container text-center">
-        <p class="mb-0">Este site usa cookies para garantir que você obtenha a melhor experiência em nosso site. <button id="acceptCookiesButton" class="btn btn-primary btn-sm">Aceitar</button></p>
+      <p class="mb-0">Este site usa cookies para garantir que você obtenha a melhor experiência em nosso site. <button
+          id="acceptCookiesButton" class="btn btn-primary btn-sm">Aceitar</button></p>
     </div>
-</div>
-<?php 
+  </div>
+  <?php
   include "footer.php";
-?>
+  ?>
