110b077e01259dcebfee09da8806522c9ea2e747
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 14:58:48 -0300

    Update

--- pages/adm.php
+++ pages/adm.php
@@ -9,14 +9,16 @@ if (!isset($_SESSION["logado"]) || $_SESSION["logado"] !== true) {
 <h2>Administração do Servidor</h2>
 <button class="btn btn-primary" type="button" onclick="toggleCollapse(1)">Botão 1</button>
 <div class="content" id="content1">
-<div class="card card-body">
-    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
-  </div>
+    <div class="card card-body">
+        Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user
+        activates the relevant trigger.
+    </div>
 </div>
 
 <button class="btn btn-primary" type="button" onclick="toggleCollapse(2)">Botão 2</button>
 <div class="content" id="content2">
-<div class="card card-body">
-    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
-  </div>
+    <div class="card card-body">
+        Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user
+        activates the relevant trigger.
+    </div>
 </div>
