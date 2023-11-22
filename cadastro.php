04edc60173561167fb084a13e15281c14acf87e4
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 14:28:52 -0300

    Update

--- cadastro.php
+++ cadastro.php
@@ -1,30 +1,30 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include_once("config/includes.php");
+  include_once("config/includes.php");
 
-    // Obtenha os dados do formulário
-    $usuario = isset($_POST["usuario"]) ? $_POST["usuario"] : "";
-    $senha = isset($_POST["senha"]) ? $_POST["senha"] : "";
-    $confirmarSenha = isset($_POST["confirmarSenha"]) ? $_POST["confirmarSenha"] : "";
-    $email = isset($_POST["email"]) ? $_POST["email"] : "";
-    $genero = isset($_POST["genero"]) ? $_POST["genero"] : "";
+  // Obtenha os dados do formulário
+  $usuario = isset($_POST["usuario"]) ? $_POST["usuario"] : "";
+  $senha = isset($_POST["senha"]) ? $_POST["senha"] : "";
+  $confirmarSenha = isset($_POST["confirmarSenha"]) ? $_POST["confirmarSenha"] : "";
+  $email = isset($_POST["email"]) ? $_POST["email"] : "";
+  $genero = isset($_POST["genero"]) ? $_POST["genero"] : "";
 
-    // Validar os dados
-    if (empty($usuario) || empty($senha) || empty($confirmarSenha) || empty($email) || empty($genero)) {
-        // Tratar erro de dados incompletos
-        echo "Preencha todos os campos do formulário.";
-        exit();
-    }
+  // Validar os dados
+  if (empty($usuario) || empty($senha) || empty($confirmarSenha) || empty($email) || empty($genero)) {
+    // Tratar erro de dados incompletos
+    echo "Preencha todos os campos do formulário.";
+    exit();
+  }
 
-    // Verificar se as senhas coincidem
-    if ($senha !== $confirmarSenha) {
-        // Tratar erro de senhas não coincidentes
-        echo "As senhas não coincidem. Tente novamente.";
-        exit();
-    }
+  // Verificar se as senhas coincidem
+  if ($senha !== $confirmarSenha) {
+    // Tratar erro de senhas não coincidentes
+    echo "As senhas não coincidem. Tente novamente.";
+    exit();
+  }
 
-    // Chamar a função cadastrar
-    cadastrar($usuario, $senha, $confirmarSenha, $email, $genero);
+  // Chamar a função cadastrar
+  cadastrar($usuario, $senha, $confirmarSenha, $email, $genero);
 }
 ?>
 
@@ -35,8 +35,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Criar Nova Conta</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="css/estilo.css">
 </head>
 
@@ -63,7 +66,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           <div class="mb-3">
             <div class="input-group">
               <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
-              <input type="text" class="form-control" id="usuario" name="usuario" placeholder="Crie um usuário" required>
+              <input type="text" class="form-control" id="usuario" name="usuario" placeholder="Crie um usuário"
+                required>
             </div>
           </div>
           <!-- Senha -->
@@ -77,7 +81,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           <div class="mb-3">
             <div class="input-group">
               <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
-              <input type="password" class="form-control" id="confirmarSenha" name="confirmarSenha" placeholder="Confirme sua senha" required>
+              <input type="password" class="form-control" id="confirmarSenha" name="confirmarSenha"
+                placeholder="Confirme sua senha" required>
             </div>
           </div>
           <!-- Email -->
@@ -106,7 +111,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
-</body>
-</html>
+  <?php
+  include "footer.php";
+  ?>
