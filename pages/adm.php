1703b8d775331996a65902f3ac5ca905f73f13b5
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-22 13:41:00 -0300

    Update

---
+++ pages/adm.php
@@ -0,0 +1,9 @@
+<?php
+// Verifica se o usuário está logado
+if (!isset($_SESSION["logado"]) || $_SESSION["logado"] !== true) {
+    // Se não estiver logado, redireciona para a página de login
+    header("Location: index.php");
+    exit();
+}
+?>
+<h2>Administração do Servidor</h2>
