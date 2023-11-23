a3936f3ae685dd321b7edad8441c67195048f608
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-23 11:49:36 -0300

    Update

--- cadastro.php
+++ cadastro.php
@@ -2,23 +2,6 @@
 include "header.php";
 ?>
 
-<!doctype html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Criar Nova Conta</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="css/estilo.css">
-  <link rel="shortcut icon" href="Favicon.ico" type="image/x-icon">
-  <script src='https://js.hcaptcha.com/1/api.js' async defer></script>
-</head>
-
 <body>
   <div class="container">
     <div class="row index-box">
