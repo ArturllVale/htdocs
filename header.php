b590cbee1e5a6e3f8b6c2388178afe1468aba92e
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-24 12:03:10 -0300

    Update

--- header.php
+++ header.php
@@ -74,35 +74,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submite']) && $_POST['
   cadastrar($usuario_c, $senha_c, $confirmarsenha_c, $email, $genero);
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit']) && $_POST['submit'] == 'recuperarSenha') {
-  $email = $_POST["email"];
-  $confirmarEmail = $_POST["confirmarEmail"];
-  recuperarSenha($email, $confirmarEmail);
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit']) && $_POST['submit'] == 'Redefinir Senha') {
-  $senha = $_POST["senha"];
-  $confirmarSenha = $_POST["confirmarSenha"];
-  $token = $_POST["token"];
-
-  // Verifica se as senhas coincidem
-  if ($senha !== $confirmarSenha) {
-      $_SESSION["erro_redefinir_senha"] = 'As senhas não coincidem.';
-  } else {
-      // Adicione lógica para atualizar a senha no banco de dados com base no token
-      if (atualizarSenhaComToken($token, $senha)) {
-          // Redireciona para a página de login após a redefinição bem-sucedida
-          header("Location: login.php");
-          exit();
-      } else {
-          $_SESSION["erro_redefinir_senha"] = 'Erro ao redefinir a senha. Tente novamente.';
-      }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (isset($_POST['submit']) && $_POST['submit'] == 'recuperarSenha') {
+      $email = $_POST["email"];
+      $confirmarEmail = $_POST["confirmarEmail"];
+      recuperarSenha($email, $confirmarEmail);
   }
 
-  // Redireciona de volta para a página de recuperação com o token
-  header("Location: recuperar_senha.php?token=$token");
-  exit();
+  if (isset($_POST['submit']) && $_POST['submit'] == 'Redefinir Senha') {
+      $senha = $_POST["senha"];
+      $confirmarSenha = $_POST["confirmarSenha"];
+      $token = $_POST["token"];
+
+      // Verifica se as senhas coincidem
+      if ($senha !== $confirmarSenha) {
+          $_SESSION["erro_redefinir_senha"] = 'As senhas não coincidem.';
+      } else {
+          // Adicione lógica para atualizar a senha no banco de dados com base no token
+          $atualizacaoSucesso = atualizarSenhaComToken($token, $senha);
+
+          if ($atualizacaoSucesso) {
+              // Redireciona para a página de login após a redefinição bem-sucedida
+              header("Location: login.php");
+              exit();
+          } else {
+              $_SESSION["erro_redefinir_senha"] = 'Erro ao redefinir a senha. Tente novamente.';
+          }
+      }
+
+      // Redireciona de volta para a página de recuperação com o token
+      header("Location: recuperar_senha.php?token=$token");
+      exit();
+  }
 }
+
 ?>
 
 <!DOCTYPE html>
--- recuperar_senha.php
+++ recuperar_senha.php
@@ -4,32 +4,38 @@ require "header.php";
 
 <body>
 
-<div class="container">
-    <div class="row index-box">
-        <div class="col-md-6 index-esquerda">
-            <h2>Informações</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper dui diam, eu euismod augue iaculis et. Nullam ante justo, faucibus eget fringilla a, maximus ut orci. Nulla justo lacus, tincidunt sed tempus ac, interdum at purus. Proin accumsan dictum tellus eu rutrum. Pellentesque sed dapibus nisi. Suspendisse tristique sodales ultrices. Donec nec ultrices lorem.</p>
-        </div>
-        <div class="col-md-6 index-direita">
-            <form action="recuperar_senha.php" method="post" id="recuperarSenha">
-                <div class="input-group mb-3">
-                    <span class="input-group-text"><i class="fas fa-envelope"></i></span>
-                    <input type="email" class="form-control" placeholder="E-mail" name="email" aria-label="E-mail" aria-describedby="basic-addon1" id="emailInput" required>
-                </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text"><i class="fas fa-envelope"></i></span>
-                    <input type="email" class="form-control" placeholder="Confirmar E-mail" name="confirmarEmail" aria-label="Confirmar E-mail" aria-describedby="basic-addon1" required>
-                </div>
-                <div class="row">
-                    <div class="col text-end">
-                        <button type="submit" name="submit" value="Redefinir Senha" class="btn btn-primary">Recuperar Senha</button>
+    <div class="container">
+        <div class="row index-box">
+            <div class="col-md-6 index-esquerda">
+                <h2>Informações</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper dui diam, eu euismod augue
+                    iaculis et. Nullam ante justo, faucibus eget fringilla a, maximus ut orci. Nulla justo lacus,
+                    tincidunt sed tempus ac, interdum at purus. Proin accumsan dictum tellus eu rutrum. Pellentesque sed
+                    dapibus nisi. Suspendisse tristique sodales ultrices. Donec nec ultrices lorem.</p>
+            </div>
+            <div class="col-md-6 index-direita">
+                <form action="recuperar_senha.php" method="post" id="recuperarSenha">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                        <input type="password" class="form-control" placeholder="Nova Senha" name="senha"
+                            aria-label="Nova Senha" aria-describedby="basic-addon1" id="senhaInput" required>
                     </div>
-                </div>
-            </form>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                        <input type="password" class="form-control" placeholder="Confirmar Senha" name="confirmarSenha"
+                            aria-label="Confirmar Senha" aria-describedby="basic-addon1" required>
+                    </div>
+                    <div class="row">
+                        <div class="col text-end">
+                            <button type="submit" name="submit" value="Redefinir Senha"
+                                class="btn btn-primary">Redefinir Senha</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
-<?php
+    <?php
     require "footer.php";
     ?>
