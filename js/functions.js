5ed9876a091e1396e6317360522d2156250ecdb1
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-28 12:34:08 -0300

    Formatando o código

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,31 @@
 // Oculta todos os conteúdos inicialmente
-window.onload = function() {
-    var allContents = document.getElementsByClassName('content');
+window.onload = function () {
+    var allContents = document.getElementsByClassName("content");
     for (var i = 0; i < allContents.length; i++) {
-      allContents[i].style.display = 'none';
+        allContents[i].style.display = "none";
     }
-  };
+};
 
-  function toggleCollapse(btnNumber) {
-    var contentId = 'content' + btnNumber;
+function toggleCollapse(btnNumber) {
+    var contentId = "content" + btnNumber;
     var content = document.getElementById(contentId);
 
     // Mostra ou oculta o conteúdo clicado
-    if (content.style.display === 'block') {
-      content.style.display = 'none';
+    if (content.style.display === "block") {
+        content.style.display = "none";
     } else {
-      // Oculta todos os conteúdos antes de mostrar o atual
-      var allContents = document.getElementsByClassName('content');
-      for (var i = 0; i < allContents.length; i++) {
-        allContents[i].style.display = 'none';
-      }
+        // Oculta todos os conteúdos antes de mostrar o atual
+        var allContents = document.getElementsByClassName("content");
+        for (var i = 0; i < allContents.length; i++) {
+            allContents[i].style.display = "none";
+        }
 
-      content.style.display = 'block';
+        content.style.display = "block";
     }
-  };
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    window.onscroll = function() {
+document.addEventListener("DOMContentLoaded", function () {
+    window.onscroll = function () {
         scrollFunction();
     };
 });
@@ -37,27 +37,27 @@ function scrollFunction() {
         topBtn.style.display = "block";
     } else {
         topBtn.style.opacity = "0";
-        setTimeout(function() {
+        setTimeout(function () {
             topBtn.style.display = "none";
         }, 400); // Tempo correspondente à transição CSS
     }
-};
+}
 
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-};
+}
 
 // Lista de classes
-var classes = ['bg1', 'bg2', 'bg3'];
+var classes = ["bg1", "bg2", "bg3"];
 
 // Função para escolher uma classe aleatória
 function escolherClasseAleatoria() {
-  return classes[Math.floor(Math.random() * classes.length)];
+    return classes[Math.floor(Math.random() * classes.length)];
 }
 
 // Obter o elemento
-var elemento = document.querySelector('.index-esquerda');
+var elemento = document.querySelector(".index-esquerda");
 
 // Remover classes existentes
 elemento.classList.remove(...classes);
