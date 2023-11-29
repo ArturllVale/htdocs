9d13d7f86f6b1701103eb219f8cf7dff1b263df5
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-29 15:19:36 -0300

    Correções

--- pages/mvps_vivo.php
+++ pages/mvps_vivo.php
@@ -7,3 +7,22 @@ if (!isset($_SESSION["logado"]) || $_SESSION["logado"] !== true) {
 }
 ?>
 <h2>MVPs Vivos</h2>
+<div class="row">
+    <?php
+    $dados = buscarDadosMvpStatus();
+    for ($i = 0; $i < count($dados); $i += 4) {
+        echo '<div class="row">';
+        for ($j = $i; $j < $i + 4; $j++) {
+            echo '<div class="col">';
+            echo '<div class="card" style="width: 18rem;">';
+            echo '<img src="data/' . strtolower($dados[$j]['mvpName']) . '.png" class="card-img-top ' . ($dados[$j]['status'] === 'MORTO' ? 'grayscale' : '') . '">';
+            echo '<div class="card-body">';
+            echo '<h5 class="card-title">' . $dados[$j]['mvpName'] . '</h5>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+        }
+        echo '</div>';
+    }
+    ?>
+</div>
