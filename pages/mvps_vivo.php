7e7e39bf0fecfe277e9558e444bf9ceb4e506be5
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-29 16:00:00 -0300

    Update

--- pages/mvps_vivo.php
+++ pages/mvps_vivo.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION["logado"]) || $_SESSION["logado"] !== true) {
         echo '<div class="row">';
         for ($j = $i; $j < $i + 4; $j++) {
             echo '<div class="col">';
-            echo '<div class="card" style="width: 18rem;">';
+            echo '<div class="card" style="width: 10rem;">';
             echo '<img src="data/' . strtolower($dados[$j]['mvpName']) . '.png" class="card-img-top ' . ($dados[$j]['status'] === 'MORTO' ? 'grayscale' : '') . '">';
             echo '<div class="card-body">';
             echo '<h5 class="card-title">' . $dados[$j]['mvpName'] . '</h5>';
