c783751938b78d03ce01d4b1139d87f1ac53043e
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-11-29 12:45:11 -0300

    BR Update

--- teste/index.php
+++ teste/index.php
@@ -8,19 +8,19 @@
     <form id="form-pagamento">
         <label for="quantidade">Quantidade de cash_coin:</label>
         <input type="number" id="quantidade" name="quantidade" min="1" required>
-
+        <br>
         <label for="num-cartao">Número do cartão:</label>
         <input type="text" id="num-cartao" name="num-cartao" required>
-
+        <br>
         <label for="cvv">CVV:</label>
         <input type="text" id="cvv" name="cvv" required>
-
+        <br>
         <label for="mes">Mês de expiração:</label>
         <input type="text" id="mes" name="mes" required>
-
+        <br>
         <label for="ano">Ano de expiração:</label>
         <input type="text" id="ano" name="ano" required>
-
+        <br>
         <input type="submit" value="Pagar">
     </form>
 
